2feff0fad990379ab4afc12d16432470af40437d
Marcos Gomez <marcos_go2015@hotmail.com>
2020-10-24 12:56:46 -0300

    menus cursos y catedras

--- src/components/catedra/Catedra.js
+++ src/components/catedra/Catedra.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const Catedra = () => {
     return (
         <div>
-            <h1>aca van a estar los ABML de las catedras</h1>
+            <div>
+            <div class="container">
+            <div class="menucentrado">
+                <div class="row mb-2">
+                    <div class="col-md-3"></div>
+                        <div class="col-md-6">
+                        <table>
+                            <tr>
+                            <th>Gestion de catedras</th>
+                            </tr>
+                            <tr>
+                            <td><Link to="/inicio">Alta catedra</Link></td>
+                            </tr>
+                            <tr>
+                            <td><Link to="/inicio">Modificar catedra</Link></td>
+                            </tr>
+                            <tr>
+                            <td><Link to="/inicio">Baja catedra</Link></td>
+                            </tr>
+                            <tr>
+                            <td><Link to="/inicio">Listado catedra</Link></td>
+                            </tr>
+                        </table>
+                        </div>
+                    <div class="col-md-3"></div>
+                </div>        
+            </div>
+            <div class="centrar">
+                <Link to="/inicio" class="btn btn-success">Atras</Link>
+            </div>
+            </div>
+        </div>
         </div>
     )
 }
--- src/components/curso/Curso.js
+++ src/components/curso/Curso.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const Curso = () => {
     return (
         <div>
-            <h1>Aca van a estar las ABML de los cursos</h1>
+            <div class="container">
+            <div class="menucentrado">
+                <div class="row mb-2">
+                    <div class="col-md-3"></div>
+                        <div class="col-md-6">
+                        <table>
+                            <tr>
+                            <th>Gestion de cursos</th>
+                            </tr>
+                            <tr>
+                            <td><Link to="/inicio">Alta curso</Link></td>
+                            </tr>
+                            <tr>
+                            <td><Link to="/inicio">Modificar curso</Link></td>
+                            </tr>
+                            <tr>
+                            <td><Link to="/inicio">Baja curso</Link></td>
+                            </tr>
+                            <tr>
+                            <td><Link to="/inicio">Listado curso</Link></td>
+                            </tr>
+                        </table>
+                        </div>
+                    <div class="col-md-3"></div>
+                </div>        
+            </div>
+            <div class="centrar">
+                <Link to="/inicio" class="btn btn-success">Atras</Link>
+            </div>
+            </div>
         </div>
     )
 }
